cbb1a3419272c7bdf21e66150d408c90bf7996df
Shawnte Elbert <drelbert@elbertinnovativesolutions.org>
2026-02-16 01:50:50 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -80,7 +80,6 @@ const getUserPortalNav = () => [
       { name: 'Program Overview', page: 'IncubateHerOverview', icon: Target },
       { name: 'Schedule Options', page: 'IncubateHerSchedule', icon: Calendar },
       { name: 'Learning Hub', page: 'IncubateHerLearning', icon: BookOpen },
-      { name: 'Agenda & Courses', page: 'IncubateHerAgenda', icon: FileText },
       { name: 'Workbook', page: 'IncubateHerWorkbook', icon: BookOpen },
       { name: 'Resource Library', page: 'ResourceLibrary', icon: FileText },
       { name: 'Video Library', page: 'IncubateHerVideoLibrary', icon: Video },
