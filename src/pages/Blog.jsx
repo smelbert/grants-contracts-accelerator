58153abded3eaa952dee8a1cd44de811ec0be6e4
Shawnte Elbert <drelbert@elbertinnovativesolutions.org>
2026-03-02 03:39:31 +0000

    File changes

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -50,7 +50,7 @@ export default function Blog() {
     <div className="min-h-screen bg-white">
       {/* Navigation */}
       <nav className="bg-white border-b border-slate-200 sticky top-0 z-50 shadow-sm">
-        <div className="max-w-7xl mx-auto px-6 py-4">
+        <div className="max-w-[1600px] mx-auto px-6 py-4">
           <div className="flex items-center justify-between">
             <Link to={createPageUrl('PublicHome')}>
               <img 
