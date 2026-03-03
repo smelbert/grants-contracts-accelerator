27fa0e71f6cf8922c0cc520b3990ec60d5620bc9
Shawnte Elbert <drelbert@elbertinnovativesolutions.org>
2026-03-03 04:23:50 +0000

    File changes

--- src/pages/BlogManagement.jsx
+++ src/pages/BlogManagement.jsx
@@ -208,7 +208,7 @@ export default function BlogManagement() {
                   <td className="px-4 py-3">
                     <div className="flex items-center justify-end gap-1">
                       {post.slug && (
-                        <Link to={createPageUrl('BlogPost') + '?slug=' + post.slug} target="_blank">
+                        <Link to={createPageUrl('BlogPost') + '?slug=' + post.slug}>
                           <Button variant="ghost" size="icon" className="h-8 w-8">
                             <Eye className="w-4 h-4" />
                           </Button>
